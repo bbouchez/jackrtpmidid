a34d3fcc8f925cea05eec6e1edd135a4dc48745b
imodularsynth <31064337+imodularsynth@users.noreply.github.com>
2020-01-30 19:38:19 +0100

    Add files via upload

---
+++ RTP_netdriver.cpp
@@ -0,0 +1,101 @@
+/* 
+ *  File:   RTP_netdriver.cpp
+ *  Author: Benoit BOUCHEZ (BEB)
+ *
+ *  Linux specific low level network functions for RTP-MIDI
+ * 
+ *  Created by Benoit BOUCHEZ on 05/12/11.
+ *  Copyright 2011 Benoit Bouchez. All rights reserved.
+ *
+ *  Licensing terms
+ *  This file and the rtpmidid project are licensed under GNU LGPL licensing terms
+ *  Use of this source code in commercial applications and/or products without
+ *  written agreement of the author is STRICTLY FORBIDDEN
+ *  Refer to LICENSE.TXT file for details
+ */
+
+#include "RTP_netdriver.h"
+#include <unistd.h>
+
+void SystemWaitMS (unsigned int MSTime)
+{
+    usleep(MSTime*1000);
+}  // SystemWaitMS
+//---------------------------------------------------------------------------
+
+bool CreateUDPSocket (TSOCKTYPE* sock, unsigned short NumPort, bool shouldReuse)
+// NumPort = port number to listen to
+{
+    long nRet;
+    sockaddr_in AdrRecv;
+    int optval;
+    int errcode;
+
+    // Create UDP/IP socket
+    *sock=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);  // IPPROTO_UDP is 0 in IPTCOM
+    if (*sock==INVALID_SOCKET) return false;
+
+    if (NumPort==0) return true;  // Do not bind the socket to a specific listening port
+	
+    if (shouldReuse)
+    {
+        optval=1;
+	errcode=setsockopt(*sock, SOL_SOCKET, SO_REUSEPORT, (char*)&optval, sizeof(optval));
+	if (errcode!=0)
+	{
+            CloseSocket(sock);
+            return false;
+	}
+    }
+
+    // Create local sink address
+    memset (&AdrRecv, 0, sizeof(sockaddr_in));
+    AdrRecv.sin_family=AF_INET;
+    AdrRecv.sin_port=htons(NumPort);
+    AdrRecv.sin_addr.s_addr=htonl(INADDR_ANY);
+
+    nRet=bind(*sock, (const sockaddr*)&AdrRecv, sizeof(AdrRecv));
+    if (nRet==-1)
+    {
+        CloseSocket(sock);
+        *sock=INVALID_SOCKET;
+        return false;
+    }
+
+    return true;  // No error
+}  // CreateUDPSocket
+//---------------------------------------------------------------------------
+
+void CloseSocket (TSOCKTYPE* sock)
+{
+    if (*sock!=INVALID_SOCKET)
+    {
+        shutdown (*sock, 2);
+        close (*sock);
+        *sock=INVALID_SOCKET;
+    }
+}  // CloseSocket
+// ----------------------------------------------------------------------------------
+
+bool DataAvail (TSOCKTYPE sock, unsigned int WaitTimeMS)
+{
+    fd_set readfds;
+    timeval timeout;
+    long result;
+
+    if (sock==INVALID_SOCKET) return false;
+
+    FD_ZERO(&readfds);
+    FD_SET(sock,&readfds);
+
+    timeout.tv_usec=WaitTimeMS*1000;
+    timeout.tv_sec=0;
+
+    result=select(sock+1, &readfds, 0, 0, &timeout);
+    if (result<0) return false;		// An error occured while processing 
+    if (result==0) return false;        // This is used when WaitTimeMS is used : it means that no data has arrived
+    if (FD_ISSET(sock, &readfds)!=0) return true;
+
+    return false;
+}  // DataAvail
+//---------------------------------------------------------------------------
---
+++ RTP_netdriver.h
@@ -0,0 +1,41 @@
+/* 
+ *  File:   RTP_netdriver.h
+ *  Author: Benoit BOUCHEZ (BEB)
+ *
+ *  Linux specific low level network functions for RTP-MIDI
+ * 
+ *  Created by Benoit BOUCHEZ on 05/12/11.
+ *  Copyright 2011 Benoit Bouchez. All rights reserved.
+ *
+ *  Licensing terms
+ *  This file and the rtpmidid project are licensed under GNU LGPL licensing terms
+ *  Use of this source code in commercial applications and/or products without
+ *  written agreement of the author is STRICTLY FORBIDDEN
+ *  Refer to LICENSE.TXT file for details
+ */
+
+#ifndef __RTP_NETDRIVER_H__
+#define __RTP_NETDRIVER_H__
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+
+#define INVALID_SOCKET			-1
+
+typedef int TSOCKTYPE;
+
+//! Makes system wait for x milliseconds
+void SystemWaitMS (unsigned int MSTime);
+
+//! Returns true if there is a datagram received on a socket and waiting to be read
+bool DataAvail (TSOCKTYPE sock, unsigned int WaitTimeMS);
+
+//! Create a UDP socket
+bool CreateUDPSocket (TSOCKTYPE* sock, unsigned short NumPort, bool shouldReuse);
+
+//! Close a socket created by CreateUDPSocket
+void CloseSocket (TSOCKTYPE* sock);
+
+#endif /* __RTP_NETDRIVER_H__ */
+
