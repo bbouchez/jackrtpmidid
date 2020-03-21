5ed449286f30334dc256a39714d603375943f3fa
imodularsynth <31064337+imodularsynth@users.noreply.github.com>
2020-03-21 20:39:01 +0100

    V0.5 update

--- jackrtpmidid.cpp
+++ jackrtpmidid.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   JackRTPMIDID.cpp
  * RTP-MIDI daemon for Jack
  * Author: Benoit BOUCHEZ
@@ -6,7 +6,7 @@
  * Created on 13 octobre 2019, 10:09
  *
  *  Licensing terms
- *  This file and the rtpmidid project are licensed under GNU LGPL licensing 
+ *  This file and the rtpmidid project are licensed under GNU LGPL licensing
  *  terms with an exception stating that rtpmidid code can be used within
  *  proprietary software products without needing to publish related product
  *  source code.
@@ -17,6 +17,14 @@
  -verbosertp : displays applemidi session messages
  */
 
+ /*
+ Release notes
+
+ V0.5 - 21/03/2020
+ - added SIGINT signal handler for proper program termination
+ - removed commented code sections from older tests
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -24,6 +32,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <math.h>
+#include <signal.h>
 
 #include <jack/jack.h>
 #include <jack/midiport.h>
@@ -32,7 +41,7 @@
 
 jack_port_t *input_port;
 jack_port_t *output_port;
-//unsigned int FrameNumber=0;
+bool break_request=false;
 
 CRTP_MIDI* RTPMIDIHandler=NULL;
 TMIDI_FIFO_CHAR MIDI2JACK;
@@ -48,10 +57,10 @@ unsigned int RTPMIDICallback (void* Instance, unsigned int DataSize, unsigned ch
     unsigned int TempInPtr;
     unsigned int ByteCount;
     bool Overflow=false;
-        
+
     CurrentOutPtr=MIDI2JACK.ReadPtr;		// Make a snapshot to avoid change during next loop
     TempInPtr=MIDI2JACK.WritePtr;			// Local copy to be updated only when full MIDI message is transferred
-	
+
     for (ByteCount=0; ByteCount<DataSize; ByteCount++)
     {
 	MIDI2JACK.FIFO[TempInPtr]=DataBlock[ByteCount];
@@ -63,7 +72,7 @@ unsigned int RTPMIDICallback (void* Instance, unsigned int DataSize, unsigned ch
             break;
 	}
     }
-	
+
     if (Overflow==false)
     {  // Update input pointer only if we have been able to store all data
         MIDI2JACK.WritePtr=TempInPtr;
@@ -75,7 +84,7 @@ unsigned int RTPMIDICallback (void* Instance, unsigned int DataSize, unsigned ch
 // Callback function called when there is an audio block to process
 int jack_process(jack_nframes_t nframes, void *arg)
 {
-    int i;
+    unsigned int i;
     void* in_port_buf = jack_port_get_buffer(input_port, nframes);
     void* out_port_buf = jack_port_get_buffer(output_port, nframes);
     jack_midi_event_t in_event;
@@ -89,40 +98,40 @@ int jack_process(jack_nframes_t nframes, void *arg)
     unsigned char SYSEXByte;
     size_t NumBytesInEvent;
     unsigned int ByteCounter;
-        
+
     jack_midi_clear_buffer(out_port_buf);    // Recommended to call this at the beginning of process cycle
-    
+
     // Check if we have MIDI data waiting in the FIFO from RTP-MIDI to be sent
     if (MIDI2JACK.ReadPtr!=MIDI2JACK.WritePtr)
     {
         // Read FIFO and generate JACK events for each MIDI message in the FIFO
         TempRead=MIDI2JACK.ReadPtr;     // Local snapshot to avoid RTP-MIDI thread to see pointer moving while we parse the buffer
         LastBufferPos=MIDI2JACK.WritePtr;
-        
+
         while (TempRead!=LastBufferPos)
         {
             // We can assume safely that there will be no incomplete MIDI message in the queue
             // as RTP-MIDI thread only transfers full MIDI messages. No need to check here
             // if a message in the queue is truncated
-            
+
             // Identify message length from first byte
             RunningStatus=MIDI2JACK.FIFO[TempRead];
-            
+
             if (RunningStatus==0xF0)
             {  // Specific SYSEX processing
                 SYSEXBuffer[0]=0xF0;
                 SYSEXSize=0;
-                
+
                 // Read SYSEX size by searching 0xF7
                 do {
                     SYSEXByte=MIDI2JACK.FIFO[TempRead];
                     TempRead+=1;
                     if (TempRead>=MIDI_CHAR_FIFO_SIZE) TempRead=0;  // Could be a mask for faster update
-                    
+
                     SYSEXBuffer[SYSEXSize]=SYSEXByte;
                     SYSEXSize++;
                 } while ((SYSEXSize<512)&&(SYSEXByte!=0xF7));
-                
+
                 // If SYSEX is too big or 0xF7 not found, reject the message
                 if (SYSEXByte==0xF7)
                 {
@@ -167,16 +176,16 @@ int jack_process(jack_nframes_t nframes, void *arg)
                 }  // Buffer to JACK allocated
             }  // Non SYSEX message
         }  // loop over all events in the queue
-        
+
         // Update read pointer only when we have parsed the whole buffer
         MIDI2JACK.ReadPtr=TempRead;
     }  // MIDI data available from RTP-MIDI queue
-        
+
     // Generate RTP-MIDI payload (with null delta-time) for each event sent by JACK
     if(event_count >= 1)
     {
         //printf("rtpmidid: %d events\n", event_count);
-        
+
         // Make a snapshot of current FIFO position
         TempWrite=JACK2RTP.WritePtr;
 
@@ -184,14 +193,14 @@ int jack_process(jack_nframes_t nframes, void *arg)
         {
             jack_midi_event_get(&in_event, in_port_buf, i);
             NumBytesInEvent=in_event.size;
-            
+
             // Try to store the event in the queue
             // Add leading null delta-time
             JACK2RTP.FIFO[TempWrite++]=0x00;
             if (TempWrite>=MIDI_CHAR_FIFO_SIZE) TempWrite=0;
             if (TempWrite==JACK2RTP.ReadPtr) return 0;      // FIFO is full discard the message
             // TODO : maybe we can save the TempWrite for the last valid message and send what has been stored successfully
-            
+
             // Copy all the bytes from the JACK MIDI message to the queue
             for (ByteCounter=0; ByteCounter<NumBytesInEvent; ByteCounter++)
             {
@@ -199,46 +208,52 @@ int jack_process(jack_nframes_t nframes, void *arg)
                 if (TempWrite>=MIDI_CHAR_FIFO_SIZE) TempWrite=0;
                 if (TempWrite==JACK2RTP.ReadPtr) return 0;      // FIFO is full discard the message
             }
-            
-            /*
-            if (*(in_event.buffer)!=0xFE)
-            {
-                printf("    event %d time is %d. 1st byte is 0x%x\n", i, in_event.time, *(in_event.buffer));
-            }
-            */
         }
-        /*	printf("1st byte of 1st event addr is %p\n", in_events[0].buffer);*/
-        
+
         // Update FIFO pointer when all events to send have been read
         JACK2RTP.WritePtr=TempWrite;
     }
-       
-    return 0;      
+
+    return 0;
 }  // jack_process
 // ----------------------------------------------------
 
 /* Callback function called when jack server is shut down */
 void jack_shutdown(void *arg)
 {
-	exit(1);
+    printf ("JACK has shut down\n");
+    break_request=true;
+	//exit(1);
 }  // jack_shutdown
+// ----------------------------------------------------
+
+void sig_handler (int signo)
+{
+    if (signo == SIGINT)
+    {
+        break_request=true;
+    }
+}  // sig_handler
 // ----------------------------------------------------
 
 int main(int argc, char** argv)
 {
     int Ret;
     jack_client_t *client;
-    
-    printf ("JACK <-> RTP-MIDI bridge V0.4 for Zynthian\n");
+
+    printf ("JACK <-> RTP-MIDI bridge V0.5 for Zynthian\n");
     printf ("Copyright 2019/2020 Benoit BOUCHEZ (BEB)\n");
     printf ("Please report any issue to BEB on https:\\discourse.zynthian.org\n");
-    
+
+    break_request=false;
+    signal (SIGINT, sig_handler);
+
     MIDI2JACK.ReadPtr=0;
     MIDI2JACK.WritePtr=0;
-    
+
     JACK2RTP.ReadPtr=0;
     JACK2RTP.WritePtr=0;
-    
+
     if (argc>=2)
     {
         if (strcmp(argv[1], "-verbosertp")==0) VerboseRTP=true;
@@ -249,7 +264,7 @@ int main(int argc, char** argv)
         fprintf(stderr, "jackrtpmidid : JACK server not running\n");
         return 1;
     }
-        
+
     RTPMIDIHandler = new CRTP_MIDI (&JACK2RTP, 0, 0, 0, &RTPMIDICallback, 0);
     if (RTPMIDIHandler)
     {
@@ -257,7 +272,7 @@ int main(int argc, char** argv)
         Ret=RTPMIDIHandler->InitiateSession (0, 5004, 5005, 5004, 5005, false);
         if (Ret==-1) fprintf (stderr, "jackrtpmidid : can not create control socket\n");
         else if (Ret==-2) fprintf (stderr, "jackrtpmidid : can not create data socket\n");
-        if (Ret!=0) 
+        if (Ret!=0)
         {
             delete RTPMIDIHandler;
             return 1;
@@ -278,22 +293,25 @@ int main(int argc, char** argv)
     }
 
     /* run until interrupted */
-    while(1)
+    while(break_request==false)
     {
         if (RTPMIDIHandler) RTPMIDIHandler->RunSession();
         SystemWaitMS(1);        // Run RTP-MIDI process every millisecond
     }
+    printf ("Program termination requested by user\n");
+
+    // Clean everything before we exit
     jack_client_close(client);
-     
     if (RTPMIDIHandler)
     {
+        printf ("Closing RTP-MIDI handler...\n");
         RTPMIDIHandler->CloseSession();
         delete RTPMIDIHandler;
         RTPMIDIHandler=0;
     }
-        
-    exit (0);
-        
+
+    printf ("Done...\n");
+
     return (EXIT_SUCCESS);
 }  // main
 // ----------------------------------------------------
